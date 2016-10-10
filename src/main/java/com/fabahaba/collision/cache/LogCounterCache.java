acc8c6fbf2ea0364b0bf96b8189e6d501f4c2faf
jamespedwards42 <james.p.edwards42@gmail.com>
2016-10-09 17:45:57 -0700

    Remove initial count subtraction from CAS increment loop.

--- src/main/java/com/fabahaba/collision/cache/LogCounterCache.java
+++ src/main/java/com/fabahaba/collision/cache/LogCounterCache.java
@@ -47,7 +47,8 @@ abstract class LogCounterCache {
       }
     }
     final double prob = initCountOffset + (1.0 / ThreadLocalRandom.current().nextDouble());
-    while (prob >= count << pow2LogFactor) {
+    //while (prob >= count << pow2LogFactor) {
+    while ((int)prob >> pow2LogFactor >= count) {
       expected = witness;
       witness = (byte) BA.compareAndExchangeRelease(counters, index, expected, (byte) (count + 1));
       if (expected == witness) {
@@ -60,36 +61,17 @@ abstract class LogCounterCache {
     }
   }
 
-  //  public static void main(String[] args) {
-  //    final int initCount = 5;
-  //    final int pow2LogFactor = LogCounterCache.calcLogFactorShift(1_048_576);
-  //    final int initCountOffset = initCount << pow2LogFactor;
-  //    for (int count = 6;count < 255;) {
-  //      final double prob = 1.0 / ThreadLocalRandom.current().nextDouble();
-  //      boolean result = prob < (count - initCount) << pow2LogFactor;
-  //      final double testProb = prob + initCountOffset;
-  //      boolean result2 = testProb < count << pow2LogFactor;
-  //      if (result != result2) {
-  //        System.err.println(result + " : " + result2);
-  //      }
-  //      if (!result) {
-  //        ++count;
-  //        //System.out.println(result);
-  //      }
-  //    }
-  //  }
-
   /**
    * Used in conjunction with {@link #atomicIncrement atomicIncrement} as a multiplication
    * factor to decrease the probability of a counter increment as the counter increases.
    *
-   * @param maxCount The relative max count.  Once a counter is incremented this many times its val
-   *                 should be 255.
+   * @param maxCount The relative max count.  Once a counter is incremented this many times its
+   *                 value should be 255.
    * @return The power of two multiplication factor as the number of bits to shift.
    */
   static int calcLogFactorShift(final int maxCount) {
     // Divide next highest power of 2 by 32,768... (256^2 / 2).
-    // Then getAggressive the number of bits to shift for efficiency.
+    // Then get the number of bits to shift for efficiency in future calculations.
     // The result of this factor will cause the count to be 255 after maxCount increments.
     return Integer.numberOfTrailingZeros(Integer.highestOneBit(maxCount - 1) >> 14);
   }
